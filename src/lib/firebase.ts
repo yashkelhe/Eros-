361685dffa937778458df0f192e6165f5fd1e583
yash kelhe <yashkelhe30@gmail.com>
2025-01-17 16:33:55 +0530

    changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = "https://ctvpuxhtzwnbkehyfiik.supabase.co"; // Replace with your Supabase project URL
-// const supabaseKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0dnB1eGh0enduYmtlaHlmaWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NDY3NTIsImV4cCI6MjA1MTQyMjc1Mn0.6SZCYkFJ5QMA6dBU_Y_hhdSjwe32U-SsU23QsE4FEkY"; // Replace with your Supabase anon key
-const supabaseUrl = process.env.SUPABASE_URL!; // Replace with your Supabase project URL
-const supabaseKey = process.env.SUPABASE_KEY!; // Replace with your Supabase anon key
+const supabaseUrl = "https://ctvpuxhtzwnbkehyfiik.supabase.co"; // Replace with your Supabase project URL
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0dnB1eGh0enduYmtlaHlmaWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4NDY3NTIsImV4cCI6MjA1MTQyMjc1Mn0.6SZCYkFJ5QMA6dBU_Y_hhdSjwe32U-SsU23QsE4FEkY"; // Replace with your Supabase anon key
+// const supabaseUrl = process.env.SUPABASE_URL!; // Replace with your Supabase project URL
+// const supabaseKey = process.env.SUPABASE_KEY!; // Replace with your Supabase anon key
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function uploadFile(
