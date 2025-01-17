bda2f70405bd97c2e7198fced70500eb2ad42ee4
yash kelhe <yashkelhe30@gmail.com>
2025-01-17 16:31:54 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default async function Home() {
-  return <div className="text-red-300">add</div>;
+  return (
+    <div className="text-red-300">
+      <Link href="/dashboard">Go to the dashboard</Link>
+    </div>
+  );
 }
